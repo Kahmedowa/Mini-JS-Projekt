efd138201fea380d1cd6ce642cf2fc3e20d06078
Kesban <k.ahmedowa94@gmail.com>
2022-09-26 11:04:20 +0200

    Projekt fertig

---
+++ index.js
@@ -0,0 +1,36 @@
+function showTime(){
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes(); 
+    let s = date.getSeconds(); 
+    
+    let session = "AM";
+    
+    if(h == 0){
+        h = 12
+    };
+    if(h > 12){
+        h = h - 12
+        session = "PM"
+    };
+    if (h < 10) {
+        h = "0" + h
+    };
+    if (m < 10) {
+        m = "0" + m
+    };
+    if (s < 10) {
+        s = "0" + s
+    };
+    
+    const time = h + ":" + m + ":" + s + " " + session;
+    
+
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    setTimeout(showTime); // Pokazva nastoqshtite sekundi 
+
+};
+    
+
+showTime();
